850f1d2373d8b8bddbf9f495c706b42dac85053b
shanks1554 <deepnagpal147514@gmail.com>
2025-06-05 12:54:13 +0530

    'commit-3'

--- application.py
+++ application.py
@@ -36,4 +36,4 @@ def home():
     return render_template('index.html', recommendations=recommendations)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
